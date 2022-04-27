69644d936f9d6f68f891b8fc422cb1168b15752d
Kim Doy <heydoyhey@gmail.com>
2022-04-28 01:11:17 +0900

    놀이공원 부족금액 계산하기 : 답 업데이트

--- attractionPrices.playground/Contents.swift
+++ attractionPrices.playground/Contents.swift
@@ -8,17 +8,19 @@
 import Foundation
 
 func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64{
-    var answer:Int64 = 0
+//    var answer:Int64 = 0
+//
+//    for index in 1...count {
+//        answer += Int64((index * price))
+//    }
+//    if money < answer {
+//        answer -= Int64(money)
+//    }
+//    else {
+//        answer = 0
+//    }
     
-    for index in 1...count {
-        answer += Int64((index * price))
-    }
-    if money < answer {
-        answer -= Int64(money)
-    }
-    else {
-        answer = 0
-    }
+    // 1~n까지 숫자의 합은 (n+1)*n/2 ㅏ
     
-    return answer
+    return max(0, Int64((count+1)*count/2*price - money))
 }
